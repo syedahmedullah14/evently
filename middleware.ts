e7b379202b75a2d55f487c8ec465db185676ebc7
Ahmedullah Jaser Syed <syedahmedullahjaser@gmail.com>
2024-05-25 22:32:23 +0530

    first commit

---
+++ middleware.ts
@@ -0,0 +1,24 @@
+import { authMiddleware } from "@clerk/nextjs/server";
+
+export default authMiddleware({
+    publicRoutes:
+    [
+        '/',
+        '/events/:id',
+        '/api/webhook/clerk',
+        '/api/webhook/stripe',
+        'api/uploadthing'
+    ],
+    ignoredRoutes:
+    [
+        '/api/webhook/clerk',
+        '/api/webhook/stripe',
+        'api/uploadthing'
+    ]
+
+});
+
+export const config = {
+
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+};
