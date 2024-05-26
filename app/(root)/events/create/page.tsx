270d31bb18522ed06886e6d30f391049a269b7ad
Ahmedullah Jaser Syed <syedahmedullahjaser@gmail.com>
2024-05-26 14:46:29 +0530

    changed db

--- app/(root)/events/create/page.tsx
+++ app/(root)/events/create/page.tsx
@@ -8,7 +8,7 @@ const CreateEvent = () => {
 
   return (
     <>
-      <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+      <section className="pl-3 bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
         <h3 className="wrapper h3-bold text-center sm:text-left">Create Event</h3>
       </section>
 
--- lib/database/index.ts
+++ lib/database/index.ts
@@ -10,7 +10,7 @@ export const connectToDatabase = async () => {
   if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
 
   cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
-    dbName: 'evently',
+    dbName: 'event_platform',
     bufferCommands: false,
   })
 
